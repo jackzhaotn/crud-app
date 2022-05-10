4e66cac13e974e391919556fca6bb17877c45a8b
jackzhaotn <jackzhaotn@gmail.com>
2022-05-09 21:58:41 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+db = SQLAlchemy(app)
+
+class Inventory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(300), nullable=False)
+
+    def __rep__(self):
+        return '<Item %r>' % self.id
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
