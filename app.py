6de03045eefe54c0f0055026b053bc3dd6d5369c
jackzhaotn <jackzhaotn@gmail.com>
2022-05-10 12:30:50 -0700

    add locations and location relationship to items

--- app.py
+++ app.py
@@ -8,19 +8,32 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 class Inventory(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(300), nullable=False)
-    qty = db.Column(db.Integer, nullable=False)
+    __tablename__ = 'items'
+    item_id = db.Column(db.String(300), primary_key=True)
+    item_qty = db.Column(db.Integer, nullable=False)
+    location_id = db.Column(db.String(300), db.ForeignKey('locations.location_id'))
+
+    location = db.relationship('Location', foreign_keys=location_id)
 
     def __rep__(self):
         return '<Item %r>' % self.id
 
+class Location(db.Model):
+    __tablename__ = 'locations'
+    location_id = db.Column(db.Integer, primary_key=True)
+    location_name = db.Column(db.String(300))
+    
+
+    def __repr__(self):
+        return '<Location %r>' % self.location_id
+
 @app.route('/', methods=['POST','GET'])
 def home():
-    if request.method == 'POST':
-        item_name = request.form['name']
-        item_qty = request.form['qty']
-        new_item = Inventory(name=item_name, qty=item_qty)
+    if (request.method == 'POST') and ('item_name' in request.form):
+        item_name = request.form['item_name']
+        item_qty = request.form['item_qty']
+        location = request.form['location']
+        new_item = Inventory(item_id=item_name, item_qty=item_qty, location_id=location)
 
         try:
             db.session.add(new_item)
@@ -28,14 +41,28 @@ def home():
             return redirect('/')
         except:
             return 'There was an error while adding the item'
+    
+    if (request.method == "POST") and ('location_name' in request.form):
+        location_name    = request.form["location_name"]
+        new_location     = Location(location_name=location_name)
 
+        try:
+            db.session.add(new_location)
+            db.session.commit()
+            return redirect("/")
+        
+        except:
+            return "There Was an issue while add a new Location"
     else:
         items = Inventory.query.all()
-        return render_template("index.html", items=items)
+        locations = Location.query.all()
+        return render_template("index.html", items=items, locations=locations)
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    item_to_delete = Inventory.query.get_or_404(id)
+
+
+@app.route('/delete/<string:item_id>')
+def delete(item_id):
+    item_to_delete = Inventory.query.get_or_404(item_id)
     try:
         db.session.delete(item_to_delete)
         db.session.commit()
@@ -44,13 +71,14 @@ def delete(id):
         return 'There was an error while deleting that item'
 
 
-@app.route('/update/<int:id>', methods=['GET','POST'])
-def update(id):
-    item = Inventory.query.get_or_404(id)
+@app.route('/update/<string:item_id>', methods=['GET','POST'])
+def update(item_id):
+    item = Inventory.query.get_or_404(item_id)
 
     if request.method == 'POST':
-        item.name = request.form['name']
-        item.qty = request.form['qty']
+        item.item_name = request.form['item_name']
+        item.item_qty = request.form['item_qty']
+        item.location = request.form['location']
 
         try:
             db.session.commit()
@@ -60,6 +88,33 @@ def update(id):
 
     else:
         return render_template('update.html', item=item)
+
+@app.route('/deleteLocation/<string:location_id>')
+def deleteLocation(location_id):
+    location_to_delete = Location.query.get_or_404(location_id)
+    try:
+        db.session.delete(location_to_delete)
+        db.session.commit()
+        return redirect('/locations/')
+    except:
+        return 'There was an error while deleting that location'
+
+
+@app.route('/updateLocation/<string:location_id>', methods=['GET','POST'])
+def updateLocation(location_id):
+    location = Location.query.get_or_404(location_id)
+
+    if request.method == 'POST':
+        location.location_name = request.form['location_name']
+    
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue while updating that task'
+
+    else:
+        return render_template('updateLocation.html', location=location)
 
 if __name__ == "__main__":
     app.run(debug=True)
