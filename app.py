7dc6afd104d3458e5efff7eb1e09c580855e6217
jackzhaotn <jackzhaotn@gmail.com>
2022-05-09 22:20:17 -0700

    add CRUD functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -14,9 +14,49 @@ class Inventory(db.Model):
     def __rep__(self):
         return '<Item %r>' % self.id
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def home():
-    return render_template("index.html")
+    if request.method == 'POST':
+        item_content = request.form['content']
+        new_item = Inventory(content=item_content)
+
+        try:
+            db.session.add(new_item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an error while adding the item'
+
+    else:
+        items = Inventory.query.all()
+        return render_template("index.html", items=items)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    item_to_delete = Inventory.query.get_or_404(id)
+    try:
+        db.session.delete(item_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was an error while deleting that item'
+
+
+@app.route('/update/<int:id>', methods=['GET','POST'])
+def update(id):
+    item = Inventory.query.get_or_404(id)
+
+    if request.method == 'POST':
+        item.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue while updating that task'
+
+    else:
+        return render_template('update.html', item=item)
 
 if __name__ == "__main__":
     app.run(debug=True)
